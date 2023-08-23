feae48b3275b3f658a3961cbc116a9a086fed484
Sunti <saknsaza87@gmail.com>
2023-08-23 20:25:10 +0700

    Update installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codeigniter4/appstarter',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '259ca2511c596df195f6c23259762c8a2ea800c0',
+        'reference' => '625e0fdbb106147755d033c328ca9b67bd6c8dff',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'codeigniter4/appstarter' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '259ca2511c596df195f6c23259762c8a2ea800c0',
+            'reference' => '625e0fdbb106147755d033c328ca9b67bd6c8dff',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
