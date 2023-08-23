448ba10168c25ea0e3f02355ad7fadaf53b49c54
Sunti <saknsaza87@gmail.com>
2023-08-23 20:36:29 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5;url=/public">
+    <title>index</title>
+</head>
+<body>
+    <a href="/public/"></a>
+    <h1>redirect</h1>
+</body>
+</html>
